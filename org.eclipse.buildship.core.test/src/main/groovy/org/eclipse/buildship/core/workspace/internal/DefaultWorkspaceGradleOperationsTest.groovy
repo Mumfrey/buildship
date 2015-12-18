facc37b237f59b71004ca827f68a149dedae4c70
Donat Csikos <csdonat@gmail.com>
2015-12-18 22:58:49 +0100

    Set target Java runtime and target language level on Java projects

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/DefaultWorkspaceGradleOperationsTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/DefaultWorkspaceGradleOperationsTest.groovy
@@ -1,5 +1,6 @@
 package org.eclipse.buildship.core.workspace.internal
 
+import org.gradle.api.JavaVersion
 import org.eclipse.buildship.core.CorePlugin
 import org.eclipse.buildship.core.configuration.GradleProjectNature
 import org.eclipse.buildship.core.configuration.internal.ProjectConfigurationPersistence
@@ -159,6 +160,7 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
             file 'sample-project/build.gradle', """
                 apply plugin: 'java'
                 sourceCompatibility = 1.2
+                targetCompatibility = 1.3
             """
             file 'sample-project/settings.gradle'
             folder 'sample-project/src/main/java'
@@ -170,9 +172,9 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
         javaProject = JavaCore.create(findProject('sample-project'))
 
         then:
-        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaCore.VERSION_1_2
+        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaVersion.current().toString()
         javaProject.getOption(JavaCore.COMPILER_SOURCE, true) == JavaCore.VERSION_1_2
-        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_2
+        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_3
     }
 
     def "If workspace project exists at model location, then an existing java project's source folders are updated"() {
@@ -314,6 +316,7 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
             file 'sample-project/build.gradle', """
                 apply plugin: "java"
                 sourceCompatibility = 1.4
+                targetCompatibility = 1.5
             """
         }
 
@@ -330,9 +333,9 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
         javaProject = JavaCore.create(findProject('sample-project'))
 
         then:
-        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaCore.VERSION_1_4
+        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaVersion.current().toString()
         javaProject.getOption(JavaCore.COMPILER_SOURCE, true) == JavaCore.VERSION_1_4
-        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_4
+        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_5
     }
 
     def "If .project file exists at the model location, then build commands and natures are set"() {
@@ -484,6 +487,7 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
             file 'sample-project/build.gradle', """
                 apply plugin: "java"
                 sourceCompatibility = 1.3
+                targetCompatibility = 1.4
             """
             file 'sample-project/settings.gradle'
             folder 'sample-project/src/main/java'
@@ -495,9 +499,9 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
 
         then:
         def javaProject = JavaCore.create(findProject('sample-project'))
-        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaCore.VERSION_1_3
+        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaVersion.current().toString()
         javaProject.getOption(JavaCore.COMPILER_SOURCE, true) == JavaCore.VERSION_1_3
-        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_3
+        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_4
     }
 
     def "If no workspace project or .project file exists, then the additional natures and build commands are set"() {
@@ -621,6 +625,7 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
             file 'sample-project/build.gradle', """
                 apply plugin: "java"
                 sourceCompatibility = 1.3
+                targetCompatibility = 1.4
             """
             file 'sample-project/settings.gradle'
             folder 'sample-project/src/main/java'
@@ -632,9 +637,9 @@ class DefaultWorkspaceGradleOperationsTest extends BuildshipTestSpecification {
 
         then:
         def javaProject = JavaCore.create(findProject('sample-project'))
-        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) == JavaCore.VERSION_1_3
+        javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true) ==  JavaVersion.current().toString()
         javaProject.getOption(JavaCore.COMPILER_SOURCE, true) == JavaCore.VERSION_1_3
-        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_3
+        javaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true) == JavaCore.VERSION_1_4
     }
 
     def "If the .project file is deleted on import, then the additional natures and build commands are set"() {
